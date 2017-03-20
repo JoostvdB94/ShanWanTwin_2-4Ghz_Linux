350ac0151c65e7e50d72f66567a0cccdafdd89c2
wanderingconfused <jbaglien@hotmail.com>
2017-03-20 14:41:29 -0500

    edit to accept multiple hidraw devices

--- start_shanwan.sh
+++ start_shanwan.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-HIDRAW_DEVICE=$(dmesg | grep -oP '(?<=2563:0555\.\d{4}: input,)\w{6}\d')
+HIDRAW_DEVICE=($(dmesg | grep -oP '(?<=2563:0555\.\d{4}: input,)\w{6}\d'))
 SCRIPT_PATH=/opt/ShanWanTwin_2-4Ghz_Linux/shanwan-joystick.py
 
 if [ -n "$HIDRAW_DEVICE" ]
-	then
-		echo "Found joystick on /dev/$HIDRAW_DEVICE"
-		exec "sudo" "python" "$SCRIPT_PATH" "/dev/$HIDRAW_DEVICE" &
+        then
+                for ((i=0; i<${#HIDRAW_DEVICE[*]}; i++));
+                        do
+                        echo "Found joystick on /dev/${HIDRAW_DEVICE[$i]}"
+                        exec "sudo" "python" "$SCRIPT_PATH" "/dev/${HIDRAW_DEVICE[$i]}" &
+                done
 fi
